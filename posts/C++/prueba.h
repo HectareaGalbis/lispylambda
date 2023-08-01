4e3bec49de6cf554e4f992bd732c3756f60a0acb
Hectarea <hectometrocuadrado@gmail.com>
2023-08-01 21:54:13 +0200

    Added collections section

--- posts/C++/prueba.h
+++ posts/C++/prueba.h
@@ -102,6 +102,18 @@ template<typename N>
 using add1_t = typename add1<N>::type;
 
 
+/// sub1
+template<typename N>
+struct sub1 : int_constant<N::value - 1> {};
+
+template<typename N>
+constexpr int sub1_v = sub1<N>::value;
+
+template<typename N>
+using sub1_t = typename sub1<N>::type;
+
+
+
 /// eql
 template<typename N, typename M>
 struct eql : bool_constant<N::value == M::value> {};
@@ -243,24 +255,57 @@ template<typename P, typename Q>
 using dot_product_t = typename dot_product<P,Q>::type;
 
 
-/// array
+/// integral_array
 template<typename T, T... TS>
-struct array{
+struct integral_array{
 	static constexpr T value[] = {TS...};
-	using type = array;
+	using type = integral_array;
 	using value_type = T;
 };
 
 template<typename T, T... TS>
-using array_t = typename array<T,TS...>::type;
+constexpr T integral_array<T, TS...>::value[];
+
+template <typename T, T... TS>
+constexpr const T* integral_array_v = integral_array<T,TS...>::value;
+
+template<typename T, T... TS>
+using integral_array_t = typename integral_array<T,TS...>::type;
+
+
+/// integral_array (char)
+template<char... TS>
+struct integral_array<char,TS...>{
+	static constexpr char value[] = {TS...,'\0'};
+	using type = integral_array;
+	using value_type = char;
+};
+
+template <char... TS>
+constexpr char integral_array<char,TS...>::value[];
+
 
 
 /// int_array
 template<int... TS>
-struct int_array : array<int,TS...> {};
+struct int_array : integral_array<int,TS...> {};
+
+template <int... TS>
+constexpr const int* int_array_v = int_array<TS...>::value;
 
 template<int... TS>
 using int_array_t = typename int_array<TS...>::type;
+
+
+/// char_array
+template<char... cs>
+struct char_array : integral_array<char,cs...> {};
+
+template <char... cs>
+constexpr const char* char_array_v = char_array<cs...>::value;
+
+template<char... cs>
+using char_array_t = typename char_array<cs...>::type;
 
 
 /// aref
@@ -275,8 +320,153 @@ template<typename A, typename N>
 using aref_t = typename aref<A,N>::type;
 
 
+/// sum_array
+template<typename A, typename N>
+struct sum_array : add<
+						aref_t<A,sub1_t<N>>,
+						typename sum_array<A,sub1_t<N>>::type> {};
+
+template<typename A>
+struct sum_array<A,int_constant_t<0>> : int_constant_t<0> {};
+
+template<typename A, typename N>
+constexpr int sum_array_v = sum_array<A,N>::value;
+
+template<typename A, typename N>
+using sum_array_t = typename sum_array<A,N>::type;
 
 
+/// int_collection
+/// define una coleccion de enteros
+template<int... ns>
+struct int_collection {
+	using type = int_collection;
+};
+
+template<int... ns>
+using int_collection_t = typename int_collection<ns...>::type;
 
 
+/// range_collection
+/// devuelve una coleccion de enteros de 0 a n
+template<int n, int... ns>
+struct range_collection_aux : range_collection_aux<n-1,n-1,ns...> {};
 
+template<int... ns>
+struct range_collection_aux<0,ns...> : int_collection<ns...> {};
+
+template<int n>
+struct range_collection : range_collection_aux<n> {};
+
+template<int n>
+using range_collection_t = typename range_collection<n>::type;
+
+
+/// aref_str
+/// devuelve el caracter de un string de C++ con indice i
+template<const char* str, int i>
+struct aref_str{
+	static constexpr char value = str[i];
+};
+
+template<const char* str, int i>
+constexpr char aref_str_v = aref_str<str,i>::value;
+
+
+/// length_str
+/// devuelve la longitud de un string de C++
+template<const char* str, int i, bool nullchar>
+struct length_str_aux : length_str_aux<str,i+1,str[i]=='\0'> {};
+
+template<const char* str, int i>
+struct length_str_aux<str,i,true>{
+	static constexpr int value = i;
+};
+
+template<const char* str>
+struct length_str : length_str_aux<str,0,str[0]=='\0'> {};
+
+template<const char* str>
+constexpr int length_str_v = length_str<str>::value;
+
+
+/// Crea un string de T++ a partir de uno de C++
+template<const char* str, typename C>
+struct make_string_aux {};
+
+template<const char* str, int... ns>
+struct make_string_aux<str,int_collection<ns...>> : integral_array<char,aref_str_v<str,ns>...> {};
+
+template<const char* str>
+struct make_string : make_string_aux<str,range_collection_t<length_str_v<str>>> {};
+
+template<const char* str>
+constexpr const char* make_string_v = make_string<str>::value;
+
+template<const char* str>
+using make_string_t = typename make_string<str>::type;
+
+constexpr char holaMundo[] = "Hola mundo!";
+
+
+/// collection
+template<typename... TS>
+struct collection{
+	using type = collection;
+};
+
+template<typename... TS>
+using collection_t = typename collection<TS...>::type;
+
+
+/// is_empty
+template<typename C>
+struct is_empty {};
+
+template<typename T, typename... TS>
+struct is_empty<collection<T,TS...>> : bool_constant<false> {};
+
+template<typename... TS>
+struct is_empty<collection<TS...>> : bool_constant<true> {};
+
+template<typename C>
+constexpr bool is_empty_v = is_empty<C>::value;
+
+template<typename C>
+using is_empty_t = typename is_empty<C>::type;
+
+
+/// concatenate
+template<typename C, typename D>
+struct concatenate {};
+
+template<typename... CS, typename... DS>
+struct concatenate<collection<CS...>,collection<DS...>> : collection<CS...,DS...> {};
+
+template<typename C,typename D>
+using concatenate_t = typename concatenate<C,D>::type;
+
+
+/// debug
+template <typename>
+struct debug {};
+
+
+/// reverse_collection
+template<typename C, typename D>
+struct reverse_collection_aux {};
+
+template<typename... cs, typename... ds>
+struct reverse_collection_aux<collection<cs...>, collection<ds...>>
+	: collection_t<ds...> {};
+
+template<typename c, typename... cs, typename... ds>
+struct reverse_collection_aux<collection<c,cs...>, collection<ds...>>
+	: reverse_collection_aux<collection_t<cs...>, collection_t<c,ds...>> {};
+
+template<typename C>
+struct reverse_collection
+	: reverse_collection_aux<C, collection_t<>> {};
+
+template<typename C>
+using reverse_collection_t = typename reverse_collection<C>::type;
